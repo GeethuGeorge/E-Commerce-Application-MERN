5369b35d4def5d28e9ec8d82861ad7df9c530ddb
Geethu George <109959204+GeethuGeorge@users.noreply.github.com>
2024-02-08 19:21:10 +0100

    media query updated

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -29,6 +29,7 @@ const NewCollections = () => {
                             image={item.image}
                             new_price={item.new_price}
                             old_price={item.old_price}
+                            className={item}
                         />
                     );
                 })}
